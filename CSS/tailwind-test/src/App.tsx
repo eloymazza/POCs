4bb8c3539dce359f4d27d1537dbbdedd357cd47c
Eloy Mazza <eloymazza.ra@gmail.com>
2024-09-16 20:21:41 +0200

    Added Tailwind POC

---
+++ CSS/tailwind-test/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold font">Test</h1>
+      <div>
+        <button className="bg-blue-500 text-white font-bold py-3 px-4 rounded hover:bg-blue-700">
+          Button
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
